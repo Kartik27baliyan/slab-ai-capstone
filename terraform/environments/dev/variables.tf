a1b59fa850e11da09a8553808cd9662e1a79c2fc
KartikBaliyan <thekartikbaliyan12@gmail.com>
2025-09-21 22:58:26 +0530

    Add Terraform variable declarations

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -21,3 +21,15 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "slab-ai-dev"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
